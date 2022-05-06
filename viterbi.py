b6e23f47efdaaa2f35ae7c4352010ea3a13e3d2b
Carter <shavitzc@msoe.edu>
2022-05-06 11:52:41 -0500

    viterbi additions

--- viterbi.py
+++ viterbi.py
@@ -45,3 +45,29 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
         prev = V[t + 1][prev]["prev"]
 
     return opt
+
+
+
+def Viterbi1(words, train_bag, tags_df, word_given_tag):
+    state = []
+    T = list(set([pair[1] for pair in train_bag]))
+     
+    for key, word in enumerate(words):
+        #initialise list of probability column for a given observation
+        p = [] 
+        for tag in T:
+            if key == 0:
+                transition_p = tags_df.loc['.', tag]
+            else:
+                transition_p = tags_df.loc[state[-1], tag]
+                 
+            # compute emission and state probabilities
+            emission_p = word_given_tag(words[key], tag)[0]/word_given_tag(words[key], tag)[1]
+            state_probability = emission_p * transition_p    
+            p.append(state_probability)
+             
+        pmax = max(p)
+        # getting state for which probability is maximum
+        state_max = T[p.index(pmax)] 
+        state.append(state_max)
+    return list(zip(words, state))
